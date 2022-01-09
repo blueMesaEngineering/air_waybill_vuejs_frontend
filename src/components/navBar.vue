6a159b4b343499ee1ce4c3f83fdb94a393e0c354
Nathan <nathan@bluemesaengineering.com>
2022-01-09 18:56:11 -0500

    Feat: Working on Navbar links

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -3,13 +3,13 @@
     <span>
       <img src = "../assets/logo.png" style="height: 5vh; width 5vh; margin: .75vh">
     </span>
-    <span>
+    <span style = "padding: 1vh .5vw 1vh .5vw; text-align: center; float: right">
       Link 1
     </span>
-    <span>
+    <span style = "padding: 1vh .5vw 1vh .5vw; text-align: center; float: right">
       Link 2
     </span>
-    <span>
+    <span style = "padding: 1vh .5vw 1vh .5vw; text-align: center; float: right">
       Link 3
     </span>
   </div>
